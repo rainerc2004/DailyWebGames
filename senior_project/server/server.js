f3c558ea3a65b255ad54665ad967fb2eb71c7cab
DHintz03 <davidhintz@ufl.edu>
2026-03-09 21:13:11 -0400

    update routes

--- senior_project/server/server.js
+++ senior_project/server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import games from "./routes/game.js";
+import playlists from "./routes/playlist.js";
+import users from "./routes/user.js";
+import friends from "./routes/friend.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/game", games);
+app.use("/playlist", playlists);
+app.use("/user", users);
+app.use("/friend", friends);
 
 // start the Express server
 app.listen(PORT, () => {
