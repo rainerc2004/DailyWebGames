6185f6a407b457a4f3fdebb2badd4fca8dec4e86
rainerc2004 <rainerc2004@gmail.com>
2026-03-11 01:02:16 -0400

    Added more functional sites

--- senior_project/client/src/components/PlayNavbar.jsx
+++ senior_project/client/src/components/PlayNavbar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
 import SearchBar from "./SearchBar";
 
 export default function PlayNavbar({ updateSite }) {
@@ -16,14 +15,24 @@ export default function PlayNavbar({ updateSite }) {
                         Wordle
                     </button>
                     <button
-                        onClick={() => updateSite("https://www.linkedin.com/games/tango/")}
+                        onClick={() => updateSite("https://worldle.teuteuf.fr/")}
                         className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow hover:bg-gray-400 active:scale-95 transition text-center">
-                        Tango
+                        Worldle
+                    </button>
+                    <button
+                        onClick={() => updateSite("https://wafflegame.net/daily")}
+                        className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow hover:bg-gray-400 active:scale-95 transition text-center">
+                        Waffle
                     </button>
                     <button
                         onClick={() => updateSite("https://catfishing.net/")}
                         className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow hover:bg-gray-400 active:scale-95 transition text-center">
                         Catfishing
+                    </button>
+                    <button
+                        onClick={() => updateSite("https://www.linkedin.com/games/queens")}
+                        className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow hover:bg-gray-400 active:scale-95 transition text-center">
+                        Queens
                     </button>
                 </div>
                 <NavLink className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow hover:bg-gray-400 active:scale-95 transition text-center">
