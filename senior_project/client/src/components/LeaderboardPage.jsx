5991a9b8c7cfc00c611a58607574e6ead875b69e
DHintz03 <davidhintz@ufl.edu>
2026-04-01 15:47:05 -0400

    Update game's day#

--- senior_project/client/src/components/LeaderboardPage.jsx
+++ senior_project/client/src/components/LeaderboardPage.jsx
@@ -8,15 +8,15 @@ const LeaderboardPage = () => {
             <Navbar />
             <div className="grid flex flex-col flex-1 items-start justify-start grid-cols-5 px-3">
                 <div className="flex flex-col col-span-1 flex-end justify-end gap-6 px-3 pb-6">
-                    <Leaderboard game_name="Wordle" user_name="user" day={1745}/>
-                    {<Leaderboard game_name="Octordle" user_name="user" day={1745}/> }
+                    <Leaderboard game_name="Wordle" user_name="user" day={1747}/>
+                    {<Leaderboard game_name="Octordle" user_name="user" day={1528}/> }
                 </div>
                 <div className="flex flex-col col-span-1 flex-end justify-end gap-6 px-3 pb-6">
-                    <Leaderboard game_name="Waffle" user_name="user" day={1745}/>
-                    {<Leaderboard game_name="Catfishing" user_name="user" day={1745}/>}
+                    <Leaderboard game_name="Waffle" user_name="user" day={1531}/>
+                    {<Leaderboard game_name="Catfishing" user_name="user" day={647}/>}
                 </div>
                 <div className="flex flex-col col-span-1 flex-end justify-end gap-6 px-3 pb-6">
-                    {<Leaderboard game_name="Queens" user_name="user" day={1745}/>}
+                    {<Leaderboard game_name="Queens" user_name="user" day={892}/>}
                 </div>
                 <div className="flex flex-col col-span-1 flex-end justify-end gap-6 px-3 pb-6">
                     {/*<Leaderboard />*/}
