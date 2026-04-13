66d3641f842dd18e850862e67074dd09c1a1a04f
DHintz03 <davidhintz@ufl.edu>
2026-04-13 16:20:08 -0400

    game search results fix

--- senior_project/client/src/components/GameCard.jsx
+++ senior_project/client/src/components/GameCard.jsx
@@ -2,9 +2,10 @@ export default function ProfileCard({title, game_link}) {
     return (
         <div class="grow px-6 bg-zinc-900 text-white font-semibold rounded-lg text-left">
             <div className="text-xl py-3 gap-6 flex items-center justify-between">
-                <a href="https://www.nytimes.com/games/wordle/index.html"> {title} </a>
+                <a href={game_link}> {title} </a>
                 <select class="excluded" className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-lg">
                     Add to list
+                    <option value="none">Add to a list</option>
                 </select>
             </div>
         </div>
