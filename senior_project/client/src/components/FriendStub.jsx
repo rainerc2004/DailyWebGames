fbcd3b34072e165a8217dbac4468f24ed2ad1dee
rainerc2004 <rainerc2004@gmail.com>
2026-04-14 00:01:03 -0400

    remove friend button

--- senior_project/client/src/components/FriendStub.jsx
+++ senior_project/client/src/components/FriendStub.jsx
@@ -1,7 +1,14 @@
 export default function FriendStub({ user_name }) {
+    const remove = () => {
+        //Accept friend request here.
+    };
+
     return (
-        <div class="w-full px-2 py-2 bg-gray-500 text-white font-semibold rounded-lg">
-            @{ user_name }
+        <div class="flex w-full gap-2 px-2 py-2 bg-gray-500 text-white font-semibold rounded-lg">
+            @{user_name}
+            <button class="rounded-lg bg-zinc-700 px-2" onClick={remove}>
+                Remove
+            </button>
         </div>
     );
 }
