82c6425698c352228ea69696ad627453825d1bfa
rainerc2004 <rainerc2004@gmail.com>
2026-04-14 01:44:23 -0400

    final tweak

--- senior_project/client/src/components/FriendsPanel.jsx
+++ senior_project/client/src/components/FriendsPanel.jsx
@@ -41,7 +41,7 @@ export default function FriendsPanel() {
                 </div>
             </div>
             <div class="row-span-6 grid flex flex-1 grid-cols-2 gap-6">
-                <div class="flex flex-col col-span-1 max-h-[450px] overflow-y-scroll gap-2">
+                <div class="flex flex-col col-span-1 max-h-[525px] overflow-y-scroll gap-2">
                     <div class="sticky top-0 w-full px-6 py-2 bg-zinc-700 text-white font-semibold">
                         Incoming requests
                     </div>
@@ -52,7 +52,7 @@ export default function FriendsPanel() {
                     <FriendStubPending user_name="this_user" />
                  
                 </div>
-                <div class="flex flex-col col-span-1 max-h-[450px] overflow-y-scroll gap-2">
+                <div class="flex flex-col col-span-1 max-h-[525px] overflow-y-scroll gap-2">
                     <div class="sticky top-0 w-full px-6 py-2 bg-zinc-700 text-white font-semibold">
                         Current friends
                     </div>
